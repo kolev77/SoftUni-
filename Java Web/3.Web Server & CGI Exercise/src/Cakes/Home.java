9e3c0fece8b50f35de467dd73cfcf97c5ffd97ab
kolev77 <rostislav.kolev77@gmail.com>
2018-01-20 02:49:42 +0200

    i've written a simple web server and implemented some logic for my previous exercise - web app with cgi

--- Java Web/3.Web Server & CGI Exercise/src/Cakes/Home.java
+++ Java Web/3.Web Server & CGI Exercise/src/Cakes/Home.java
@@ -4,24 +4,37 @@ public class Home {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.append("Content-Type: text/html\r\n\r\n");
-        sb.append("<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>CAKES</title>\n" +
-                "</head>");
-        sb.append("<body>\n" +
+        sb.append("<html>\r\n").append(HtmlElements.HEADER_ELEMENT).append(System.lineSeparator())
+                .append("<body>\r\n").append(
                 "<h2>Home</h2>\n" +
                 "<a href=\"/cgi-bin/menu.cgi\">Back to MENU</a>\n" +
                 "<section>\n" +
                 "    <h3 id=\"cakes\">Our cakes</h3>\n" +
-                "    <p>Cake is a form of sweet dessert that is typically baked. In its oldest forms, cakes were modifications of breads, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, and pies</p>\n" +
-                "<img src=\"/CakesImages/cake1.jpg\" alt=\"Cake image...\"width=\"600\" height=\"500\">"+
+                "    <p><strong><em>Cake</em></strong> is a form of <strong><em>sweet dessert</em></strong> that is typically baked." +
+                " In its oldest forms, cakes were modifications of breads," +
+                " but cakes now cover a wide range of" +
+                " preparations that can be simple or elaborate, " +
+                "and that share features with other desserts such as" +
+                " pastries, meringues, custards, and pies</p>\n" +
+                "<img src=\"/CakesImages/cake1.jpg\" alt=\"Cake image...\"width=\"600\" height=\"500\">\r\n" +
                 "</section>\n" +
-                "<section >\n" +
+                "<section>\n" +
                 "    <h3 id=\"stores\">Our stores</h3>\n" +
                 "    <p>Our stores are located in 21 cities all over the world. Come and see what we have for you.</p>\n" +
-                "<img src=\"/CakesImages/store.jpg\" alt=\"Store image...\"width=\"600\" height=\"500\">"+
-                "</section>" +
-                "</body>");
+                "<img src=\"/CakesImages/store.jpg\" alt=\"Store image...\"width=\"600\" height=\"500\">\r\n" +
+                "</section>\r\n")
+                .append("<h2 id=\"about-us\">About us</h2>")
+                .append("<dl>\n" +
+                        "    <dt>ByTheCake EOOD</dt>\n" +
+                        "    <dd>Name of the company</dd>\n" +
+                        "    <dt>Rostislav</dt>\n" +
+                        "    <dd>Owner</dd>\n" +
+                        "</dl>\n")
+                .append("  <pre style=\"background-color: #f94f80\">City: HongKong                City: Salzburg\n" +
+                        "Address: ChoCoLad 18          Address: SchokoLeiden 73\n" +
+                        "Phone: +78952804429           Phone: +49241432990\n" +
+                        "</pre>\n");
+        sb.append(HtmlElements.FOOTER_ELEMENT + "\r\n").append("</body>\r\n</html>");
         System.out.println(sb.toString());
     }
 }
--- Java Web/3.Web Server & CGI Exercise/src/Cakes/Menu.java
+++ Java Web/3.Web Server & CGI Exercise/src/Cakes/Menu.java
@@ -5,12 +5,12 @@ public class Menu {
         StringBuilder sb = new StringBuilder();
         sb.append("Content-Type: text/html\r\n\r\n");
         sb.append("<html>\r\n" +
-                "<head> </head>\r\n" +
-                "<header>\r\n"+
+                HtmlElements.HEADER_ELEMENT +
+                "<hr/>");
+        String homeMenu = "\r\n" +
+                "<body>\r\n" +
                 "<h1>By The Cake</h1>" +
                 "<h2>Enjoy our awesome cakes </h2>" +
-                "<hr/>"+ "\r\n</header>");
-        String homeMenu = "\r\n<body>\r\n"+
                 "<ul>\n" +
                 "    <li><a href=\"/cgi-bin/home.cgi\">Home</a></li>\n" +
                 "        <ol>\n" +
@@ -19,10 +19,11 @@ public class Menu {
                 "        </ol>\n" +
                 "    <li><a href=\"#\">Add cake</a></li>\n" +
                 "    <li><a href=\"#\">Browse cakes</a></li>\n" +
-                "    <li><a href=\"#\">About us</a></li>\n" +
+                "    <li><a href=\"/cgi-bin/home.cgi/#about-us\">About us</a></li>\n" +
                 "</ul>\n"
                 + "</body>\r\n";
         sb.append(homeMenu);
+        sb.append(HtmlElements.FOOTER_ELEMENT);
         sb.append("</html>");
         System.out.println(sb.toString());
     }
